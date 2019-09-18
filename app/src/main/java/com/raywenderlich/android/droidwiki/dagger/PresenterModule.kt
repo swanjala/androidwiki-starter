620dc46308bf4bc5c4e25fbc9f7b0ce939d4f7a1
live-beta <wanjala.sam.s@gmail.com>
2019-09-18 04:28:50 +0300

    (update): preseenter module
    - update home page presenter
    - add entry presenter provider

--- app/src/main/java/com/raywenderlich/android/droidwiki/dagger/PresenterModule.kt
+++ app/src/main/java/com/raywenderlich/android/droidwiki/dagger/PresenterModule.kt
@@ -1,7 +1,10 @@
 package com.raywenderlich.android.droidwiki.dagger
 
+import com.raywenderlich.android.droidwiki.network.Wiki
 import com.raywenderlich.android.droidwiki.ui.homepage.HomepagePresenter
 import com.raywenderlich.android.droidwiki.ui.homepage.HomepagePresenterImpl
+import com.raywenderlich.android.droidwiki.ui.search.EntryPresenter
+import com.raywenderlich.android.droidwiki.ui.search.EntryPresenterImpl
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -10,5 +13,11 @@ import javax.inject.Singleton
 class PresenterModule{
     @Provides
     @Singleton
-    fun provideHomePagePresenter():HomepagePresenter = HomepagePresenterImpl()
+    fun provideHomePagePresenter():HomepagePresenter =
+            HomepagePresenterImpl(homepage)
+
+    @Provides
+    @Singleton
+    fun provideEntryPresenter(wiki: Wiki):EntryPresenter = EntryPresenterImpl(wiki)
+
 }
