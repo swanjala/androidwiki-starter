1c1cc13e28f9c52a29beb4be42db85165df954d4
live-beta <wanjala.sam.s@gmail.com>
2019-09-18 03:52:43 +0300

    (add) app component

---
+++ app/src/main/java/com/raywenderlich/android/droidwiki/dagger/AppComponent.kt
@@ -0,0 +1,10 @@
+package com.raywenderlich.android.droidwiki.dagger
+
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [AppModule::class])
+
+interface AppComponent{
+}
