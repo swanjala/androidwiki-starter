26dbcb6457c94ab284261cc0e7b00efbb7c868ef
live-beta <wanjala.sam.s@gmail.com>
2019-09-18 04:10:00 +0300

    (add): update network module with name base url

---
+++ app/src/main/java/com/raywenderlich/android/droidwiki/dagger/NetworkModule.kt
@@ -0,0 +1,11 @@
+package com.raywenderlich.android.droidwiki.dagger
+
+import dagger.Module
+
+@Module
+class NetworkModule{
+    companion object{
+        private const val NAME_BASE_URL = "NAME_BASE_URL"
+
+    }
+}
