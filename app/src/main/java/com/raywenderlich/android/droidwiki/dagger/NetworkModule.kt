0319608e47851e48c8b523684d6035bb3e74e2df
live-beta <wanjala.sam.s@gmail.com>
2019-09-18 04:18:26 +0300

    (update): network module
    - add http client provider
    - add request builder provider
    - add wiki api provider

--- app/src/main/java/com/raywenderlich/android/droidwiki/dagger/NetworkModule.kt
+++ app/src/main/java/com/raywenderlich/android/droidwiki/dagger/NetworkModule.kt
@@ -1,9 +1,13 @@
 package com.raywenderlich.android.droidwiki.dagger
 
+import com.raywenderlich.android.droidwiki.network.WikiApi
 import com.raywenderlich.android.droidwiki.utils.Const
 import dagger.Module
 import dagger.Provides
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
 import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 class NetworkModule{
@@ -15,4 +19,17 @@ class NetworkModule{
     @Named(NAME_BASE_URL)
     fun provideBaseUrlString()= "${Const.PROTOCOL}://${Const.LANGUAGE}.${Const.BASE_URL}"
 
+    @Provides
+    @Singleton
+    fun provideHttpClient() = OkHttpClient()
+
+    @Provides
+    @Singleton
+    fun provideRequestBuilder(@Named(NAME_BASE_URL) baseUrl:String) =
+            HttpUrl.parse(baseUrl)?.newBuilder()
+
+    @Provides
+    @Singleton
+    fun provideWikiApi(client: OkHttpClient, requestBuilder:HttpUrl.Builder?) =
+            WikiApi(client, requestBuilder)
 }
