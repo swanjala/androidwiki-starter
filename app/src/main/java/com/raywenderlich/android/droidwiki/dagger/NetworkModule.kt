f2f0cbc7f8d612454c3c0338eb2bcca1e75319fb
live-beta <wanjala.sam.s@gmail.com>
2019-09-18 04:13:37 +0300

    (update): network module
    - set base url string

--- app/src/main/java/com/raywenderlich/android/droidwiki/dagger/NetworkModule.kt
+++ app/src/main/java/com/raywenderlich/android/droidwiki/dagger/NetworkModule.kt
@@ -1,6 +1,9 @@
 package com.raywenderlich.android.droidwiki.dagger
 
+import com.raywenderlich.android.droidwiki.utils.Const
 import dagger.Module
+import dagger.Provides
+import javax.inject.Named
 
 @Module
 class NetworkModule{
@@ -8,4 +11,8 @@ class NetworkModule{
         private const val NAME_BASE_URL = "NAME_BASE_URL"
 
     }
+    @Provides
+    @Named(NAME_BASE_URL)
+    fun provideBaseUrlString()= "${Const.PROTOCOL}://${Const.LANGUAGE}.${Const.BASE_URL}"
+
 }
