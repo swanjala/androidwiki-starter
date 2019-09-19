778a67f4da07ad4a49eef769ffa3ce383a91c8f1
live-beta <wanjala.sam.s@gmail.com>
2019-09-20 02:21:06 +0300

    (update): variable reference

--- app/src/main/java/com/raywenderlich/android/droidwiki/application/WikiApplication.kt
+++ app/src/main/java/com/raywenderlich/android/droidwiki/application/WikiApplication.kt
@@ -8,13 +8,16 @@ import com.raywenderlich.android.droidwiki.dagger.DaggerAppComponent
 class WikiApplication : Application() {
 
   lateinit var wikiComponent:AppComponent
+
+
   override fun onCreate() {
     super.onCreate()
     wikiComponent = initDagger(this)
+
   }
 
-  private fun initDagger(app:WikiApplication): AppComponent =
+  private fun initDagger(application:WikiApplication): AppComponent =
           DaggerAppComponent.builder()
-                  .appModule(AppModule(this))
+                  .appModule(AppModule(application))
                   .build()
 }
